176edfd18666719f0e25b7cdd091f6a8a69bad53
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2016-05-19 23:36:26 +0200

    [ADD]Step 2

---
+++ src/scores/HighScore2.java
@@ -0,0 +1,137 @@
+package scores;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+/**
+ * This is the class HighScore which allows to retrieve scores on ThingSpeak server
+ * @version 1.0
+ * @author Ricoeur-Hassan 
+ * @since 2016-05-03
+ */
+public class HighScore2 {
+	
+	String channelID;
+	String serverAdressBegin="https://api.thingspeak.com/channels/";
+	String serverAdressRequest="/feeds.csv";
+	String splitSeparator=",";
+	
+	/**
+	 * Constructor of a HighScore class object
+	 * 
+	 * @param ID a string which is the ID of your ThingSpeak channel
+	 */
+	public HighScore2(String ID) {
+		this.channelID = ID;
+	}
+	
+	/**
+	 * Function which allows to download all scores from a ThingSpeak server
+	 * 
+	 * @exception IOException if file not present on server
+	 * @exception MalformedURLException if connection impossible with server
+	 * @return ArrayList<String[]> which are couples of scores and players
+	 */
+	public ArrayList<String[]> getScore()
+	{
+		//Construction de l'adresse complète du serveur pour récupérer feeds.csv
+		String serverAdress=this.serverAdressBegin+this.channelID+this.serverAdressRequest;
+		
+		ArrayList<String[]> listScore = new ArrayList<String[]>();
+		URL myAdress = null;
+		try {
+			//Création d'un objet URL avec l'adresse du serveur ThingSpeak
+			myAdress = new URL(serverAdress);
+			try {
+				//Ouverture d'une connexion vers le serveur
+				InputStream input = myAdress.openStream();
+				InputStreamReader inputR = new InputStreamReader(input);
+				BufferedReader br=new BufferedReader(inputR);
+				
+				String ligne;
+				ligne=br.readLine();
+				int compt=0;
+				
+				//Parcours du fichier présent sur le serveur et récupération des scores
+				while (ligne!=null){
+					String[] data = ligne.split(this.splitSeparator);
+					
+					//On enleve la première ligne car c'est l'entête et on vérifie que la ligne comporte tous les champs pour enlever les lignes vides
+					if(compt>0 && data.length==4)
+					{
+						String[] insertTab = new String[2];
+						insertTab[0]=data[2];
+						insertTab[1]=data[3];
+						listScore.add(insertTab);
+					}
+					compt++;
+					
+					ligne=br.readLine();
+				}
+				br.close(); 
+				
+				
+			} catch (IOException e) {
+				System.out.println("Impossible de lire le fichier présent sur le serveur");
+			}
+		}catch(MalformedURLException me){
+		      System.out.println("Impossible d'accès aux fichiers sur le serveur");
+		}
+		
+		return listScore;
+		
+	}
+	
+	/**
+	 * Method which with the getScore method retrieves the scores and players, 
+	 * and sorts this scores in descendant order and keeps just the 10 best scores
+	 * @exception NumberFormatException if conversion string to int failed
+	 * @return ArrayList<String[]> Store 10 best couples of scores and players
+	 */
+	public ArrayList<BestPlayer2> tenBestScores(ArrayList<String[]> scoreUnranked)
+	{
+		ArrayList<BestPlayer2> scoreRanked = new ArrayList<BestPlayer2>();
+		int compt=0;
+		
+		//Tant qu'il reste des couples dans la liste des scoreUnranked et qu'on a pas les 10 meilleurs
+		while(scoreUnranked.size()>0 && compt<10)
+		{
+			ListIterator itr = scoreUnranked.listIterator();
+			String[] result =(String[]) itr.next();
+			String[] cplMax = result;
+			BestPlayer2 bp = new BestPlayer2(result[1],Integer.parseInt(result[0]));
+			
+			//On récupère le couple score,player avec le score le plus grand
+			while (itr.hasNext()){
+				try{
+					result =(String[]) itr.next();
+					BestPlayer2 p = new BestPlayer2(result[1],Integer.parseInt(result[0]));
+					if(p.compareTo(bp)==1)
+					{
+						bp=p;
+						cplMax=result;
+					}
+					
+				}
+				catch(NumberFormatException e){
+				        System.out.println("Problème survenue lors de la conversion de string en int."+e.getMessage());
+				}
+			}
+			
+			//On ajoute ce couple à la liste des scoreRanked et on le supprime des Unranked
+			scoreRanked.add(bp);
+			scoreUnranked.remove(cplMax);
+			compt++;
+		}
+		return scoreRanked;
+		
+	}
+	
+	
+}
---
+++ src/scores/TestHighScore2.java
@@ -0,0 +1,100 @@
+/**
+ * 
+ */
+package scores;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * This is the class TestHighScore which allows to select a fake score from a file for a new player
+ * @version 1.0
+ * @author Ricoeur-Hassan 
+ * @since 2016-05-03 
+ */
+public class TestHighScore2 {
+	/**
+	 * @param arg
+	 * @exception InputMismatchException if name's type input is incorrect 
+	 * @exception NumberFormatException if conversion string to int failed
+	 * @exception FileNotFoundException if problem occurred during opening
+	 * @exception IOException if problem if problem occurred during reading
+	 */
+	public static void main (String [] arg){
+		ArrayList<Integer> scoreArray= new ArrayList<Integer>();
+		String playerName="";
+		String splitSeparator=",";
+		int playerScore;
+		Scanner sc = new Scanner(System.in);
+		
+		//Création d'un nouvel objet HighScore
+		HighScore2 hg = new HighScore2("109182");
+		
+		//Récupération des 10 meilleurs scores du serveur ThingSpeak
+		ArrayList<String[]> scoresList = hg.getScore();
+		ArrayList<BestPlayer2> highscoresList = hg.tenBestScores(scoresList);
+		
+		//Affichage du tableau des 10 meilleurs scores 
+		System.out.println("Tableau des meilleurs scores");
+		System.out.println("----------------------------");
+		
+		//Parcours de la liste des meilleurs grâce à un iterateur
+		ListIterator itr = highscoresList.listIterator();
+		while (itr.hasNext())
+		{
+			BestPlayer2 result = (BestPlayer2) itr.next();
+			System.out.println(result.getPlayer()+" | "+result.getScore());
+		}
+		
+		//Demande à l'utilisateur de rentrer un nom
+		try{
+			System.out.println("Veuillez rentrer un nom de joueur :");
+			playerName = sc.nextLine();
+			System.out.println("Bonjour, "+playerName);
+		}
+		catch (java.util.InputMismatchException e){
+            System.out.println("Le nom que vous avez rentré ne correspond pas au type attendu"+e.getMessage());
+        }
+		
+		try {
+			//Ouverture du fichier txt contenant les scores
+			String file = "scoreSamples.txt";
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String ligne = br.readLine();
+			//Parcours de toutes les lignes du fichier
+			while(ligne!=null){
+			      String[] data = ligne.split(splitSeparator);
+			      try
+			      {
+			    	  //Ajout du score à la liste des scores
+			    	  scoreArray.add(Integer.parseInt(data[0]));
+			      }
+			      catch(NumberFormatException e){
+			        System.out.println("Problème survenu lors de la conversion de string en int."+e.getMessage());
+			      }
+			      ligne=br.readLine();
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Problème survenu dans l'ouverture du fichier"+e.getMessage());
+		} catch (IOException ex) {
+			System.out.println("Problème survenu dans la lecture du fichier"+ex.getMessage());
+		}
+		
+		//Génération d'un nombre random qui va aller de 0 à taille de liste des scores -1
+		Random randomGenerator = new Random();
+		int index = randomGenerator.nextInt(scoreArray.size());
+		
+		playerScore=scoreArray.get(index);
+		
+		//Affichage du nom et du score random du joueur
+		System.out.println("Félicitation "+playerName+", vous avez réalisé un score de "+playerScore);
+		
+	}
+}
